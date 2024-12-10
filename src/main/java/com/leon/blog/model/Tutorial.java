5bdc8c3a855d77fb2fa456debf575524c2aa41ba
LEON AKONDE <jeu@jeu>
2024-12-10 23:17:33 +0100

    post crud

---
+++ src/main/java/com/leon/blog/model/Tutorial.java
@@ -0,0 +1,41 @@
+package com.leon.blog.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Tutorial {
+
+	@Id
+	private String id;
+	private String name;
+	private String shortDescription;
+	private String content;
+	private String category;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+}
